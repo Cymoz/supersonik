b66360d4873deec6860c5030e5080ff963f7fc70
Chris <62357935+YouknowNothingJeanNeige@users.noreply.github.com>
2020-11-25 13:54:24 +0100

    mis en place de bootstrap

--- assets/app.js
+++ assets/app.js
@@ -9,6 +9,9 @@
 import './styles/app.scss';
 
 // Need jQuery? Install it with "yarn add jquery", then uncomment to import it.
-// import $ from 'jquery';
+import $ from 'jquery';
+
 
 console.log('Hello Webpack Encore! Edit me in assets/app.js');
+console.log($('body'));
+window.$ = $;
