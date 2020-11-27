53eb5832d9736dce08b4528fd226306a2b942e47
Chris <62357935+YouknowNothingJeanNeige@users.noreply.github.com>
2020-11-27 16:42:37 +0100

    Mis en place et test vich et liip

--- src/Entity/Member.php
+++ src/Entity/Member.php
@@ -40,7 +40,7 @@ class Member
     private $imageFile;
 
     /**
-     * @ORM\Column(type="string")
+     * @ORM\Column(type="string", nullable=true)
      * @var string|null
      */
     private $imageName;
