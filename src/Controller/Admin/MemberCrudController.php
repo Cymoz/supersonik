a4e1250e0c0e440449a652eb58d2e2b3c94bf31f
Olivier <olivier@cymoz.fr>
2020-11-27 16:43:24 +0100

    search bugs...

--- src/Controller/Admin/MemberCrudController.php
+++ src/Controller/Admin/MemberCrudController.php
@@ -4,13 +4,16 @@ namespace App\Controller\Admin;
 
 use App\Entity\Member;
 use Doctrine\ORM\EntityManagerInterface;
+use EasyCorp\Bundle\EasyAdminBundle\Config\Crud;
 use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractCrudController;
+use EasyCorp\Bundle\EasyAdminBundle\Dto\CrudDto;
 use EasyCorp\Bundle\EasyAdminBundle\Field\IdField;
 use EasyCorp\Bundle\EasyAdminBundle\Field\ImageField;
 use EasyCorp\Bundle\EasyAdminBundle\Field\TextField;
 use Liip\ImagineBundle\Imagine\Cache\CacheManager;
 use Symfony\Component\DependencyInjection\ParameterBag\ParameterBagInterface;
 use Symfony\Component\HttpFoundation\File\UploadedFile;
+use Vich\UploaderBundle\Form\Type\VichImageType;
 use Vich\UploaderBundle\Templating\Helper\UploaderHelper;
 
 class MemberCrudController extends AbstractCrudController
@@ -35,15 +38,32 @@ class MemberCrudController extends AbstractCrudController
 
     public function configureFields(string $pageName): iterable
     {
-        return [
+
+        $imageFile = ImageField::new('imageFile', 'Photo')->setFormType(VichImageType::class);
+        $imageName = ImageField::new('imageName', 'Photo')->setBasePath('public' . $this->params->get('app.path.member_images'));
+
+        $fields =  [
             IdField::new('id')->onlyOnIndex(),
             TextField::new('firstname'),
             TextField::new('lastname'),
 
             ImageField::new('imageName', 'Photo')
                 ->setUploadDir('public' . $this->params->get('app.path.member_images'))
+                //->setFormType(VichImageType::class)
+                ->setBasePath('public' . $this->params->get('app.path.member_images'))
                 ->setTemplatePath('admin/vich_uploader_image.html.twig'),
+
         ];
+
+       /* if($pageName == Crud::PAGE_INDEX || $pageName == Crud::PAGE_DETAIL){
+            $fields[] = $imageName;
+        }else{
+            $fields[] = $imageFile;
+        }*/
+
+        return $fields;
+
+
     }
 
     /**
