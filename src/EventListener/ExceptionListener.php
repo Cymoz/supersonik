8432f2518e283af7e3aa6dee0465036a57734ff1
Chris <62357935+YouknowNothingJeanNeige@users.noreply.github.com>
2020-12-15 17:08:53 +0100

    WIP handle exception

--- src/EventListener/ExceptionListener.php
+++ src/EventListener/ExceptionListener.php
@@ -4,7 +4,18 @@
 namespace App\EventListener;
 
 
+use Symfony\Component\HttpKernel\Event\ExceptionEvent;
+
 class ExceptionListener
 {
+
+    public function onKernelException(ExceptionEvent $event)
+    {
+        $exception = $event->getThrowable();
+        if ($exception->getCode() == 404) {
+            dd($exception);
+        }
+
+    }
 
 }
