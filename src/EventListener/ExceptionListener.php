a8f34cbe6a05fe79bdf177394cbf6d7023d6afb5
Chris <62357935+YouknowNothingJeanNeige@users.noreply.github.com>
2020-12-16 10:13:31 +0100

    mise en place d'un listener et envoie de mail quand une 404 est trouvé

--- src/EventListener/ExceptionListener.php
+++ src/EventListener/ExceptionListener.php
@@ -5,17 +5,35 @@ namespace App\EventListener;
 
 
 use Symfony\Component\HttpKernel\Event\ExceptionEvent;
+use Twig\Environment;
 
 class ExceptionListener
 {
-
+    private $twig;
+    private $mailer;
+    public function __construct(Environment $twig, \Swift_Mailer $mailer)
+    {
+        $this->twig = $twig;
+        $this->mailer = $mailer;
+    }
     public function onKernelException(ExceptionEvent $event)
     {
         $exception = $event->getThrowable();
         if ($exception->getCode() == 404) {
-            dd($exception);
+            $receiver = array('admin@kilkenny.fr');
+            $info = 'test of content';
+
+            $message = (new \Swift_Message('404 from website'))
+                ->setTo($receiver)
+                ->setSender('leo@kilkenny.fr')
+                ->setReplyTo('admin@kilkenny.fr')
+                ->setBody(
+                    $this->twig->render('mail/admin_404.html.twig',
+                        array('info' => $info)
+                    ),
+                    'text/html'
+                );
+            $this->mailer->send($message);
         }
-
     }
-
 }
