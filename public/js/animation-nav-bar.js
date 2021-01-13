98d9937c2b418a828f483cfde5cec7500047682a
workMiksTou <guillaume.lamaison33@gmail.com>
2021-01-13 17:41:40 +0100

    dernières modifications

--- public/js/animation-nav-bar.js
+++
@@ -1,7 +0,0 @@
-console.log("test");
-$(function () {
-    $(document).scroll(function () {
-        let $nav = $(".navbar-fixed-top");
-        $nav.toggleClass('scrolled', $(this).scrollTop() > $nav.height());
-    });
-});
