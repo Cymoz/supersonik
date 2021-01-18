252c754b5603eb2a9b350a01410eb03c20e8aaaf
workMiksTou <guillaume.lamaison33@gmail.com>
2021-01-18 16:05:55 +0100

    reprendre overlay expertise

--- assets/app.js
+++ assets/app.js
@@ -12,6 +12,6 @@ import 'bootstrap/dist/js/bootstrap.bundle';
 // Need jQuery? Install it with "yarn add jquery", then uncomment to import it.
 import $ from 'jquery';
 
-
+console.log("test");
 
 window.$ = $;
